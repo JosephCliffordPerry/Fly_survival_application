37a48fe422cd4dcb5e7bfec1a864172ce7291668
Perry <jp19193@essex.ac.uk>
2025-10-10 12:25:50 +0100

    script to run app made properly

--- load_app.R
+++ load_app.R
@@ -1,3 +1,11 @@
-install.packages("flySurvivalApp_0.0.0.9000.tar.gz", repos = NULL, type = "source")
+# Install devtools if not already installed
+install.packages("devtools")
+
+# Load devtools
+library(devtools)
+
+
+devtools::install_github("https://github.com/JosephCliffordPerry/Fly_survival_application.git", ref = "Package")
+
 library(flySurvivalApp)
 Load_fly_app()
